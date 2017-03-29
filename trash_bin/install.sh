09673b68fe11876d15c8f46af2528d3f02f3fcc6
stark-zhang <2623825899@qq.com>
2017-03-29 21:33:20 +0800

    add install limitation

--- trash_bin/install.sh
+++ trash_bin/install.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
 # Output some information
-echo "The trash bin is being installed...."
 
-# Copy the files of Trash Bin to /bin
-cp -r 
+# Declare some paremeters
+install_path="/bin"
+
+# Check the user permission
+# If this user is Root, the script will be executed.
+# If not, the script will exit.
+if [ $UID != 0 ]; then
+    echo "You need root access to run it!"
+    exit 1
+else
+    echo "The Trash Bin is being installed!"
+    # Give scripts execution permission
+    chmod +x ./*
+    # Copy scripts to /bin/
+    cp -p ./* $install_path
+    echo "Completed!"
+fi 
+
 # EOF
